8df425427df8071b5a07ad1d71d1787d59af8060
WeiWang <1428206861@qq.com>
2024-09-01 00:19:51 +0800

    海报栏左右按钮设计完成

--- AdvertiseBoard.cpp
+++ AdvertiseBoard.cpp
@@ -4,17 +4,43 @@
 #include<QPaintevent>
 #include<QPainter>
 #include<QPainterPath>
+#include<QMouseEvent>
 
 static const int G_Radius = 5;  //圆球半径
 static const int G_Space = 2 * G_Radius;   //圆球之间的间隔
 static int posterIndex = 0;
+
 AdvertiseBoard::AdvertiseBoard(QWidget *parent)
     : QWidget{parent}
     ,m_timer(new QTimer(this))
+    ,m_leftLab(new MyLLabel(this))
+    ,m_rightLab(new MyRLabel(this))
 {
     connect(this->m_timer,&QTimer::timeout,this,[this]{posterIndex = (++posterIndex) % this->m_posters.size();update();});
     this->m_timer->start(3000);
 
+    this->m_leftLab->setObjectName("leftLab");
+    this->m_rightLab->setObjectName("rightLab");
+
+    this->m_leftLab->setPixmap(QPixmap(":/image/window/left.svg"));
+    this->m_rightLab->setPixmap(QPixmap(":/image/window/right.svg"));
+
+    this->m_leftLab->hide();
+    this->m_rightLab->hide();
+
+    this->m_leftLab->setFixedWidth(60);
+    this->m_rightLab->setFixedWidth(60);
+    // 设置标签背景透明
+    this->m_leftLab->setAttribute(Qt::WA_TranslucentBackground);
+    this->m_rightLab->setAttribute(Qt::WA_TranslucentBackground);
+    // 设置居中
+    this->m_leftLab->setAlignment(Qt::AlignCenter);
+    this->m_rightLab->setAlignment(Qt::AlignCenter);
+
+    connect(this->m_leftLab, &MyLLabel::clicked, [this] {posterIndex = (posterIndex - 1 + this->m_posters.size()) % this->m_posters.size(); this->update(); });
+    connect(this->m_rightLab, &MyRLabel::clicked, [this] {posterIndex = (posterIndex + 1) % this->m_posters.size(); this->update(); });
+
+    updateLabPosition();
 }
 
 AdvertiseBoard::~AdvertiseBoard()
@@ -29,6 +55,15 @@ void AdvertiseBoard::addPoster(const QPixmap &posterPix)
 {
     QPixmap* pix = new QPixmap(posterPix);
     this->m_posters.append(pix);
+}
+
+void AdvertiseBoard::updateLabPosition()
+{
+    this->m_leftLab->setFixedHeight(this->height());
+    this->m_rightLab->setFixedHeight(this->height());
+    
+    this->m_leftLab->move(0, 0);
+    this->m_rightLab->move(this->width() - this->m_rightLab->width(), 0);
 }
 
 void AdvertiseBoard::paintEvent(QPaintEvent *ev)
@@ -49,7 +84,6 @@ void AdvertiseBoard::paintEvent(QPaintEvent *ev)
     //绘制广告图
     painter.drawPixmap(this->rect(),img);
 
-
     //绘制广告播放进度
     auto s = this->m_posters.size();
     auto drawStartPos = QPoint((this->width() - (s-1) * (G_Radius * 2 + G_Space)) / 2,this->height() - 10);
@@ -63,3 +97,23 @@ void AdvertiseBoard::paintEvent(QPaintEvent *ev)
     }
     QWidget::paintEvent(ev);
 }
+
+void AdvertiseBoard::resizeEvent(QResizeEvent* ev)
+{
+    QWidget::resizeEvent(ev);
+    updateLabPosition();
+}
+
+void AdvertiseBoard::enterEvent(QEnterEvent* ev)
+{
+    this->m_leftLab->show();
+    this->m_rightLab->show();
+}
+
+void AdvertiseBoard::leaveEvent(QEvent* ev)
+{
+    this->m_leftLab->hide();
+    this->m_rightLab->hide();
+}
+
+
--- AdvertiseBoard.h
+++ AdvertiseBoard.h
@@ -3,8 +3,61 @@
 
 #include <QWidget>
 #include<QList>
+#include<QLabel>
+
 class QPixmap;
 class QTimer;
+class MyLLabel : public QLabel {
+    Q_OBJECT
+
+public:
+    MyLLabel(QWidget* parent = nullptr) : QLabel(parent) {
+        setMouseTracking(true);
+    }
+signals:
+    void clicked();
+protected:
+    void enterEvent(QEnterEvent* event) override {
+        setPixmap(QPixmap(":/image/window/left-green.svg")); // 悬停时的图片
+        QLabel::enterEvent(event);
+    }
+
+    void leaveEvent(QEvent* event) override {
+        setPixmap(QPixmap(":/image/window/left.svg")); // 悬停时的图片
+        QLabel::leaveEvent(event);
+    }
+
+    void mousePressEvent(QMouseEvent* event) override {
+        emit clicked();
+        QLabel::mousePressEvent(event);
+    }
+};
+class MyRLabel : public QLabel {
+    Q_OBJECT
+
+public:
+    MyRLabel(QWidget* parent = nullptr) : QLabel(parent) {
+        setMouseTracking(true);
+    }
+signals:
+    void clicked();
+
+protected:
+    void enterEvent(QEnterEvent* event) override {
+        setPixmap(QPixmap(":/image/window/right-green.svg")); // 悬停时的图片
+        QLabel::enterEvent(event);
+    }
+
+    void leaveEvent(QEvent* event) override {
+        setPixmap(QPixmap(":/image/window/right.svg")); // 悬停时的图片
+        QLabel::leaveEvent(event);
+    }
+
+    void mousePressEvent(QMouseEvent* event) override {
+        emit clicked();
+        QLabel::mousePressEvent(event);
+    }
+};
 
 class AdvertiseBoard : public QWidget
 {
@@ -14,13 +67,19 @@ public:
     ~AdvertiseBoard();
 public:
     void addPoster(const QPixmap& posterPix);
-
+    void updateLabPosition();
 protected:
     void paintEvent(QPaintEvent* ev)override;
+    void resizeEvent(QResizeEvent* ev)override;
+    void enterEvent(QEnterEvent* ev)override;
+    void leaveEvent(QEvent* ev)override;
 
 private:
     QList<QPixmap*>m_posters;
+    MyLLabel* m_leftLab{};
+    MyRLabel* m_rightLab{};
     QTimer* m_timer{};
 };
+
 
 #endif // ADVERTISEBOARD_H
