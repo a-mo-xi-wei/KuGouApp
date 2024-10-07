1a33525986df0aea0f708f9f9d88d4ddb982bde7
WeiWang <1428206861@qq.com>
2024-10-07 23:58:04 +0800

    本想让水平滑动条提升，后面想了想还是算了

--- SliderWidget/SliderWidget.cpp
+++ SliderWidget/SliderWidget.cpp
@@ -13,34 +13,48 @@ const int SliderWidget::getValue() const {
 }
 
 void SliderWidget::mousePressEvent(QMouseEvent *event) {
-    // 获取鼠标点击相对于滑动条的位置
-    int pos = orientation() == Qt::Vertical ? event->position().y() : event->position().x();
-    // 获取滑动条的可用长度 (即滑块可以滑动的范围)
-    int sliderLength = this->orientation() == Qt::Vertical ? this->height() : this->width();
-    // 计算点击位置相对于整个滑动条的百分比
-    double percentage = static_cast<double>(pos) / sliderLength;
-    if(this->orientation() == Qt::Vertical)percentage = 1 - percentage;
-    // 根据百分比计算出滑动条的新值
-    this->m_currentValue = this->m_minValue + percentage * (this->m_maxValue - this->m_minValue);
-    // 设置滑动条的新值
-    this->setValue(this->m_currentValue);
-    emit noVolume(this->m_currentValue == 0);
-    //qDebug()<<"m_currentValue : "<<this->m_currentValue;
-    // 正在按下
-    this->m_isPressing = true;
+    if(orientation() == Qt::Horizontal) {
+        // 获取鼠标点击相对于滑动条的位置
+        int pos = event->position().x();
+        // 获取滑动条的可用长度 (即滑块可以滑动的范围)
+        int sliderLength = this->width();
+        // 计算点击位置相对于整个滑动条的百分比
+        double percentage = static_cast<double>(pos) / sliderLength;
+        // 根据百分比计算出滑动条的新值
+        this->m_currentValue = this->m_minValue + percentage * (this->m_maxValue - this->m_minValue);
+        qDebug()<<"水平 "<< this->m_currentValue << " percentage "<<percentage;
+        // 设置滑动条的新值
+        this->setValue(this->m_currentValue);
+    }else {
+        //qDebug()<<"垂直";
+        int pos = event->position().y();
+        // 获取滑动条的可用长度 (即滑块可以滑动的范围)
+        int sliderLength = this->height();
+        // 计算点击位置相对于整个滑动条的百分比
+        double percentage = static_cast<double>(pos) / sliderLength;
+        percentage = 1 - percentage;
+        // 根据百分比计算出滑动条的新值
+        this->m_currentValue = this->m_minValue + percentage * (this->m_maxValue - this->m_minValue);
+        // 设置滑动条的新值
+        this->setValue(this->m_currentValue);
+        emit noVolume(this->m_currentValue == 0);
+        //qDebug()<<"m_currentValue : "<<this->m_currentValue;
+        // 正在按下
+        this->m_isPressing = true;
+    }
     // 让父类处理其余的鼠标事件
     QSlider::mousePressEvent(event);
 }
-
 void SliderWidget::mouseMoveEvent(QMouseEvent *event) {
+    //qDebug()<<"垂直";
     if(this->m_isPressing) {
         // 获取鼠标移动相对于滑动条的位置
-        int pos = orientation() == Qt::Vertical ? event->position().y() : event->position().x();
+        int pos = event->position().y();
         // 获取滑动条的可用长度
-        int sliderLength = this->orientation() == Qt::Vertical ? this->height() : this->width();
+        int sliderLength = this->height();
         // 将鼠标位置转换为滑动条值
         double percentage = static_cast<double>(pos) / sliderLength;
-        if(this->orientation() == Qt::Vertical)percentage = 1 - percentage;
+        percentage = 1 - percentage;
         // 根据百分比计算出滑动条的新值
         this->m_currentValue = this->m_minValue + percentage * (this->m_maxValue - this->m_minValue);
         // 设置滑动条的新值
@@ -66,9 +80,9 @@ void SliderWidget::showEvent(QShowEvent *event) {
     this->m_parent = qobject_cast<MyWidget*>(this->parent());
     // 确保 m_parent 不为空再进行连接
     if (this->m_parent)
-        connect(this->m_parent,&MyWidget::noVolume,this,[this](bool flag){onNoVolume(flag);});
-}
+        connect(this->m_parent,&MyWidget::noVolume,this,&SliderWidget::onNoVolume);
 
+}
 void SliderWidget::onNoVolume(bool flag) {
     if(flag) {
         this->setValue(0);
