2d80f2b5832da5eada090e05c43f731ec03a7c44
WeiWang <1428206861@qq.com>
2024-10-13 23:19:08 +0800

    提升playWidget,调节渐变色

---
+++ TitlePlayWidget/TitleWidget.cpp
@@ -0,0 +1,29 @@
+//
+// Created by WeiWang on 24-9-18.
+//
+
+#include "TitleWidget.h"
+#include<QPainter>
+
+TitleWidget::TitleWidget(QWidget *parent)
+    : QWidget(parent) {
+}
+
+void TitleWidget::mouseDoubleClickEvent(QMouseEvent *event) {
+    QWidget::mouseDoubleClickEvent(event);
+    if (event->button() == Qt::LeftButton) {
+        emit doubleClicked(); // 发送自定义的双击信号
+    }
+}
+
+void TitleWidget::paintEvent(QPaintEvent *ev) {
+    QWidget::paintEvent(ev);
+    QPainter p(this);
+    p.setPen(Qt::NoPen);
+    // 创建一个线性渐变
+    QLinearGradient gradient(rect().topLeft(), rect().bottomLeft());
+    gradient.setColorAt(0, QColor("#87CEFA")); // 起始颜色
+    gradient.setColorAt(1, QColor("#eef2ff")); // 结束颜色
+    p.setBrush(gradient);
+    p.drawRoundedRect(rect(), 8, 8);
+}
