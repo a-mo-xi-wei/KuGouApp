d67059a07644a564ebf60c57d40d99ab5db3c1a8
WeiWang <1428206861@qq.com>
2024-10-14 09:17:54 +0800

    调色完成

--- BasicWidget/CenterWidget.cpp
+++ BasicWidget/CenterWidget.cpp
@@ -17,9 +17,19 @@ void CenterWidget::paintEvent(QPaintEvent *ev) {
     QLinearGradient gradient(rect().topLeft(), rect().bottomRight());
     gradient.setColorAt(0, QColor("#eef2ff")); // 起始颜色
     gradient.setColorAt(0.25, QColor("#dff1ff")); // 中间颜色
-    gradient.setColorAt(0.5, QColor("#e8ffd4")); // 中间颜色
+    gradient.setColorAt(0.5, QColor("#d8f9ff")); // 中间颜色
     gradient.setColorAt(0.75, QColor("#dff1ff")); // 中间颜色
     gradient.setColorAt(1, QColor("#eef2ff")); // 结束颜色
     p.setBrush(gradient);
     p.drawRoundedRect(rect(), 8, 8);
+    // 设置组合模式为透明度混合
+    //p.setCompositionMode(QPainter::CompositionMode_Multiply);
+    //gradient = QLinearGradient(rect().topRight(), rect().bottomLeft());
+    //gradient.setColorAt(0, QColor("#eef2ff")); // 起始颜色
+    //gradient.setColorAt(0.25, QColor("#dff1ff")); // 中间颜色
+    //gradient.setColorAt(0.5, QColor("#e8ffd4")); // 中间颜色
+    //gradient.setColorAt(0.75, QColor("#dff1ff")); // 中间颜色
+    //gradient.setColorAt(1, QColor("#eef2ff")); // 结束颜色
+    //p.setBrush(gradient);
+    //p.drawRoundedRect(rect(), 8, 8);
 }
